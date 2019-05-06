a4ec8d4f7b685977780179a514e84a8969d5bf41
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-05-06 15:45:10 -0700

    Edit board and styling

--- js/main.js
+++ js/main.js
@@ -1,34 +1,35 @@
 /*----- constants -----*/
         // store value of each cell
 var onOff = {
-    "0": "white",
-    "1": "black"
-}
+    '0': 'white',
+    '1': 'black'
+};
+
+// store value of puzzle solution
+var solution = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 0
+[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0], // column 1
+[0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0], // column 2
+[0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0], // column 3
+[0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0], // column 4
+[0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1], // column 5
+[0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1], // column 6
+[0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0], // column 7
+[0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0], // column 8
+[0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0], // column 9
+[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0] // column 10
+];
 
 /*----- app's state variables -----*/
 var board, winner;
-// store value of puzzle solution
-
-var solution = [
-[0, 0, 0, 0, 0, 1, 1, 1, 1, 0] // column 0
-[1, 0, 0, 0, 1, 1, 0, 0, 0, 0] // column 1
-[0, 1, 0, 1, 1, 1, 1, 1, 1, 0] // column 2
-[0, 1, 1, 1, 0, 1, 1, 0, 1, 0] // column 3
-[0, 0, 0, 1, 1, 1, 1, 0, 0, 1] // column 4
-[0, 0, 0, 1, 1, 1, 1, 0, 0, 1] // column 5
-[0, 1, 1, 1, 0, 1, 1, 0, 1, 0] // column 6
-[0, 1, 0, 1, 1, 1, 1, 1, 1, 0] // column 7
-[1, 0, 0, 0, 1, 1, 0, 0, 0, 0] // column 8
-[0, 0, 0, 0, 0, 1, 1, 1, 1, 0] // column 9
-];
 
 /*----- cached element references -----*/
-var message = document.querySelector("");
+// var message = document.querySelector("");
 
 /*----- event listeners -----*/
-document.querySelector("section").addEventListener("click", handleClick);
-document.getElementById("check").addEventListener("click", checkSolution);
-document.getElementById("play").addEventListener("click", displayKey);
+// document.querySelector("section").addEventListener("click", handleClick);
+// document.getElementById("check").addEventListener("click", checkSolution);
+// document.getElementById("play").addEventListener("click", displayKey);
 
 /*----- functions -----*/
 initialize();
@@ -45,23 +46,31 @@ function checkSolution() {
 
         // display empty clickable grid
 function render() {
+    //display the board
+    board.forEach(function(colArr, colIdx) {
+        colArr.forEach(function(cell, rowIdx) {
+            // access the correct div in each section
+            const div = document.getElementById(`c${colIdx}r${rowIdx}`);
+            div.style.backgroundColor = onOff[cell];
+        });
+    });
 
 }
-
 
         // initialize empty board
 function initialize() {
     board = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 0
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 1
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 2
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 3
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 4
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 5
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 6
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 7
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 8
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 9
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 0
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 2
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 3
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 4
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 5
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 6
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 7
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 8
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 9
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // column 10
     ];
     winner = null;
     render();
