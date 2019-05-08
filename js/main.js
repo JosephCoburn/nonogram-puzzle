b518c9086e3b9a2d1842074e19f613d615dedf94
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-05-08 11:04:30 -0700

    Flip tiles, Change values, Fix solution orientation

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 /*----- constants -----*/
     // Value Color pairs for each cell
-const COLORS = {
-    '0': 'white',
-    '1': 'black',
-};
+// const COLORS = {
+//     '0': 'white',
+//     '1': 'black',
+// };
 
     // Store puzzle solution
 const solution = [
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 
-[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0], // column 1
-[0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0], // column 2
-[0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0], // column 3
-[0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0], // column 4
-[0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1], // column 5
-[0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1], // column 6
-[0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0], // column 7
-[0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0], // column 8
-[0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0], // column 9
-[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0] // column 10
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 0
+[0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0], // column 1
+[0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0], // column 2
+[0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0], // column 3
+[0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0], // column 4
+[0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0], // column 5
+[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // column 6
+[0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1], // column 7
+[0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1], // column 8
+[0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1], // column 9
+[0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0] // column 10
 ];
 
 /*----- app's state variables -----*/
@@ -37,32 +37,56 @@ initialize();
     // process shading activity
 function handleClick(evt) {
    const marker = evt.target;
-   const colIdx = marker.id.replace('col', '');
-    // IF USING ABOVE AND NO ID'S FOR KEYS - ADD PARSEINT AND::: 
-      // if (isNaN(colIdx)) return;
-    // UPDATE TO FIND EXACT POSITION INSTEAD OF FIRST ZERO::: 
-      // const rowIdx = board[colIdx].indexOf(zero);
-    board[colIdx][rowIdx] = turn;
-    turn = -1;
+    var markerString = marker.id;
+    var rowrArr = markerString.split('r');
+    var rowIdx = rowrArr[rowrArr.length-1];
+    rowrArr.pop();
+    var colIdx = rowrArr.join('').replace('c', '');
+    // console.log(colIdx)
+    if(colIdx != 0){
+
+        if(!marker.style.backgroundColor){
+            marker.style.backgroundColor = 'black'
+            board[rowIdx][colIdx] = 1
+        } else {
+            marker.style.backgroundColor = ''
+            board[rowIdx][colIdx] = 0
+        }
+        
+    }
+    checkWin()
+
+    // var colIdx = marker.id.substring(1,2);
+    // var rowIdx = marker.id.split('r')[marker.id.split('').length -1 ];
+
+//    console.log("column index", colIdx, "row index", rowIdx);
+   console.log(board);
+   
+    // UPDATE TO FIND EXACT POSITION AT INDEXOF::: 
+    // const rowIdx = board[colIdx].indexOf();
+
+    // board[colIdx][rowIdx] = turn;
+    // turn = -1;
     render();
 }
 
     // check if current board array === solution array
 function checkWin() {
-    if (board === solution) {
+    if (board.toString() === solution.toString()) {
         alert('You win!');
     }
 }
 
 function render() {
-    // Display the board
-    board.forEach(function(colArr, colIdx) {
-        colArr.forEach(function(cell, rowIdx) {
-    // Access the correct div in the section
-            const div = document.getElementById(`c${colIdx}r${rowIdx}`);
-            div.style.backgroundColor = COLORS[cell];
-        });
-    });
+    // Display the board            
+    // const div = document.getElementById(`c${colIdx}r${rowIdx}`);
+    //     div.style.backgroundColor = COLORS[cell];
+    // board.forEach(function(colArr, colIdx) {
+    //     colArr.forEach(function(cell, rowIdx) {
+    // // // Access the correct div in the section
+
+    //     });
+    // });
     // Display win message
     if (winner) {} else {}
 }
