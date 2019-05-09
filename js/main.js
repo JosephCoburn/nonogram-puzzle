438e47dcb8eeaaad320632ab33dae7fe520e561b
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-05-09 13:30:36 -0700

    Fix bugs and cleanup

--- js/main.js
+++ js/main.js
@@ -1,73 +1,60 @@
-document.getElementsByTagName('h1')
-
-
 /*----- constants -----*/
 
-    // Store puzzle solution
+// Store puzzle solution
 const solution = [
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 0
-[0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0], // column 1
-[0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0], // column 2
-[0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0], // column 3
-[0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0], // column 4
-[0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0], // column 5
-[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // column 6
-[0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1], // column 7
-[0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1], // column 8
-[0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1], // column 9
-[0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0] // column 10
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 0
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0], // column 1
+    [0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0], // column 2
+    [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0], // column 3
+    [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0], // column 4
+    [0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0], // column 5
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // column 6
+    [0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1], // column 7
+    [0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1], // column 8
+    [0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1], // column 9
+    [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0] // column 10
 ];
 
 /*----- app's state variables -----*/
 var board;
 
 /*----- cached element references -----*/
+var headline = document.getElementById("headline");
 
 /*----- event listeners -----*/
 document.getElementById('gameBoard')
-  .addEventListener('click', handleClick);
-
-// var myBoard = document.querySelectorAll('div');
-// for (i = 0; i < myBoard.length; i++) { 
-//     myBoard[i].style.backgroundColor = "white"
-//   }
+  .addEventListener('mousedown', handleClick);
 
 /*----- functions -----*/
 initialize();
 
-// process shading activity and flip binary values
 function handleClick(evt) {
-   const marker = evt.target;
+    const marker = evt.target;
     var markerString = marker.id;
     var rowArr = markerString.split('r');
-    var rowIdx = rowArr[rowArr.length-1];
+    var rowIdx = rowArr[rowArr.length - 1];
     rowArr.pop();
     var colIdx = rowArr.join('').replace('c', '');
-    if(colIdx != 0){
-        console.log(marker.style.backgroundColor)
-        if (!marker.style.backgroundColor){
+    if (colIdx != 0) {
+        if (!marker.style.backgroundColor) {
             marker.style.backgroundColor = 'black'
             board[rowIdx][colIdx] = 1;
         } else if (marker.style.backgroundColor = 'black') {
             marker.style.backgroundColor = ''
-            board[rowIdx][colIdx] = 0
+            board[rowIdx][colIdx] = 0;
         }
     }
     checkWin()
-    render();
 }
 
 // Check if current board array matches solution array
 function checkWin() {
     if (board.toString() === solution.toString()) {
-        document.getElementById("headline").innerHTML = "You Win!";
-        document.getElementById("headline").style.fontSize = "70px";
+        headline.innerHTML = "You Win!";
+        headline.style.fontSize = "70px";
     }
 }
 
-// Display the board
-function render() {           
-}
 
 // Initialize board
 function initialize() {
@@ -84,5 +71,4 @@ function initialize() {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // column 9
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  // column 10
     ];
-    render();
 }
